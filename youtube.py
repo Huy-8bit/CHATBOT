2ccccd720f46849bffda222f853cbc78042ee7da
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2022-11-23 23:45:53 +0700

    new

--- youtube.py
+++ youtube.py
@@ -2,12 +2,15 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
+
 import numpy
 import tflearn
 import tensorflow
 import random
 import json
 import pickle
+
+
 
 with open("intents.json") as file:
     data = json.load(file)
