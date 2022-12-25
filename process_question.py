98ab01a2fa8bb1b6bdd8a839fbe3926dbdf0517b
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2022-12-25 13:53:55 +0700

    build data question.json

--- process_question.py
+++ process_question.py
@@ -10,43 +10,14 @@ def get_structure(messages_input):
     result = word_tokenize(messages_input)
     return result
 
-def find_classify_key(structure, key_classify):
-    for i in range(len(structure)):
-        if structure[i][1] == key_classify:
-            return structure[i][0]
-    return ""
+
 
 
 
 def get_answer(messages_input, data):
-    # Tokenize the input message
-    structure = get_structure(messages_input)
-    # Convert the tokens to lowercase
-    lower_structure = [t.lower() for t in structure]
-    # Loop through the data categories
-    for category, category_data in data.items():
-        # Loop through the patterns in each category
-        for item in category_data:
-            patterns = item["patterns"]
-            # Split the patterns string into a list of patterns
-            patterns_list = patterns.split(", ")
-            # Loop through the patterns in the list
-            for pattern in patterns_list:
-                # Check if any of the patterns are contained in the lowercase tokens
-                if pattern in lower_structure:
-                    # Return the corresponding response if a pattern is found
-                    return item["responses"]
-    # Return an empty string if no patterns are found
-    return ""
-
-
-
-# def process_message_question(messages_input):
-#     print("process_message_question")
-#     structure = get_structure(messages_input)
-#     print("structure: ", structure)
-    
-#     return "Đang tìm câu trả lời"
+    # Get the structure of the message
+    print(data)
+    return "NULL"
 
 
 
@@ -54,23 +25,28 @@ def get_answer(messages_input, data):
 def process_message_question(messages_input, data):
     # Get the answer to the question
     answer = get_answer(messages_input, data)
-    if answer:
-        # Return the answer if it is found
-        return answer
-    else:
-        # Return a default message if no answer is found
-        return "Sorry, I couldn't find an answer to that question."
+    return answer
 
+
+def check_data(data):
+    message = "Tôi muốn hỏi khi nào bắt đầu mở cửa ngân hàng?"
+        
 
 def test():
-    messages_input = "Tôi muốn hỏi khi nào mở cửa ngân hàng?"
+    messages_input = "Tôi muốn hỏi khi nào bắt đầu mở cửa ngân hàng?"
     
     print("messages_input: ", messages_input)
     classify = sentence_classification.analysis_message(messages_input)
     print("classify: ", classify[0])
     data = sentence_classification.load_data('question.json')
-    result = process_message_question(messages_input, data)
-    print("result: ", result)
+    # result = process_message_question(messages_input, data)
+    check_data(data)
+    # print("result: ", result)
     
+    
+
+
     
 test()
+
+
