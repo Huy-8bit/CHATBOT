34d27e51e76a79c1daf21b6d431b249ce655b59b
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2022-12-26 22:40:34 +0700

    update

--- process_question.py
+++ process_question.py
@@ -14,39 +14,78 @@ def get_structure(messages_input):
 
 
 
-def get_answer(messages_input, data):
-    # Get the structure of the message
-    print(data)
+
+
+
+
+def get_important_key(messages_input,data):
+    list_key = word_tokenize(messages_input)
+    classes = ""
+    key = ""
+    for key in list_key:
+        for value in data['classes']:
+            if key in value['key']:
+                classes = value['classes']
+                key = key
+    type = ""
+    for key in list_key:
+        for value in data['quection']:
+            if key in value['key']:
+                type = value['type']
+    return classes, key, type            
+
+
+def get_response(classes, key, type, data):
+    response = "NULL"
+    # for value in data['data']:
+    #     print(value['patterns'])
+    return response
+
+
+def get_answer( classes, key, type, data):
+    return get_response(classes, key, type, data)
+
+
+
+def check_story(messages_input, classify):
+    print("Checking story")
+    
+    
+    data = sentence_classification.load_data('story.json')
+    for value in data['data']:
+        if classify in value['tag']:
+            for result in value['story']:
+                if messages_input in result['patterns']:
+                    return result['responses'][0]
+        else:
+            print("Don't have this story")
     return "NULL"
-
-
-
 
 def process_message_question(messages_input, data):
     # Get the answer to the question
-    answer = get_answer(messages_input, data)
+    
+    classes, key, type = get_important_key(messages_input, data)
+    response = get_response(classes, key, type, data)
+    answer = get_answer(classes, key, type, data)
     return answer
 
 
-def check_data(data):
-    message = "Tôi muốn hỏi khi nào bắt đầu mở cửa ngân hàng?"
-        
+
 
 def test():
-    messages_input = "Tôi muốn hỏi khi nào bắt đầu mở cửa ngân hàng?"
+    messages_input = "tôi muốn hỏi về địa điểm của ngân hàng?"
     
     print("messages_input: ", messages_input)
     classify = sentence_classification.analysis_message(messages_input)
     print("classify: ", classify[0])
-    data = sentence_classification.load_data('question.json')
-    # result = process_message_question(messages_input, data)
-    check_data(data)
-    # print("result: ", result)
-    
+    result = check_story(messages_input, classify[0])
+    if result == "NULL":
+        data = sentence_classification.load_data('question.json')
+        result = process_message_question(messages_input, data)
+    print("bot: ", result)
     
 
 
     
 test()
-
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+    # for value in data['data']:
+    #     print(value['patterns'])
