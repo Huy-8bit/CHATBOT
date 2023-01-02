a2f4208d1386688d0432b33ef894ca0bf0bb2623
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-01-02 21:12:16 +0700

    new

--- process_question.py
+++ process_question.py
@@ -9,9 +9,9 @@ import sentence_classification
 def get_structure(messages_input):
     result = word_tokenize(messages_input)
     return result
-         
+
+
 def check_story(messages_input, classify):
-    print("Checking story")
     data = sentence_classification.load_data('story.json')
     for value in data['data']:
         if classify in value['tag']:
@@ -22,34 +22,81 @@ def check_story(messages_input, classify):
     return "NULL"
 
 
-def get_important_key(messages_input,data):
+def find_responses(messages_input, data):
+    print("Finding responses")
+    key = []
+    response = []
+    subject = []
+    for value in data[0]:
+        if value['subject'] in messages_input:
+            print("value: ", value)
+            for keys in value['key']:
+                if keys in messages_input:
+                    key.append(keys)
+                    index = value['key'].index(keys)
+                    response.append(value['responses'][index])
+                    subject = value['subject']
+    return response, key, subject
+
+
+def build_answer(messages_input, data, classes, type):
+    print("Building answer")
+    messages = get_structure(messages_input)
+    new_data = []
+    response, key, subject = [], [], []
+    answer = "chưa hiểu câu hỏi của bạn!"
+    for value in data['table']:
+        if classes in value['classes']:
+            new_data.append(value['data'])
+            response, key, subject = find_responses(messages_input, new_data)
+            answer = ""
+            for i in range(0, len(response) - 1):
+                answer += key[i] + " của " + subject + \
+                    " là " + response[i] + ", "
+            answer += key[len(response) - 1] + " của " + \
+                subject + " là " + response[len(response) - 1]
+            return answer
+
+    return answer
+
+
+def get_important_key(messages_input, data):
     print("")
     print("Getting important key")
     stuct_message = get_structure(messages_input)
     print("struct: ", stuct_message)
     print("")
     classes = "NULL"
-    for value in data['classes']:
-        print(value['key']) 
-        if classes in value['key']:
-            classes = value['classes']
-            break
-            
-    print("classes: ", classes)
+    type = "NULL"
+    for word in stuct_message:
+        for value in data['classes']:
+            if word in value['key']:
+                classes = value['classes'][0]
+                break
+        for value in data['quection']:
+            if word in value['key']:
+                type = value['type'][0]
+                break
+
+    return classes, type
+
 
 def process_message_question(messages_input, data):
     # Get the answer to the question
     print("")
     print("Processing question")
-    get_important_key(messages_input, data)
-    return "Chưa biết trả lời câu hỏi này!!!"    
+    classes, type = get_important_key(messages_input, data)
+    if classes != "NULL" or type != "NULL":
 
-
-
+        result = build_answer(messages_input, data, classes, type)
+        return result
+    else:
+        return "Chưa biết trả lời câu hỏi này!!!"
 
 
 def test():
-    messages_input = "tôi muốn hỏi về địa chỉ của ngân hàng vietcombank?"
+    messages_input = "số điện thoại và địa chỉ của techcombank là gì?"
+    messages_input = messages_input.lower()
     # messages_input = "khi nào mở cửa ngân hàng?"
     print("messages_input: ", messages_input)
     classify = sentence_classification.analysis_message(messages_input)
@@ -59,11 +106,8 @@ def test():
     if result == "NULL":
         data = sentence_classification.load_data('question.json')
         result = process_message_question(messages_input, data)
-        
+    print("")
     print("bot: ", result)
-    
 
 
-    
 test()
-
--- test.py
+++
@@ -1,5 +0,0 @@
-import sentence_classification
-
-import json
-
-# lower string file data
