a1fb749f0c5bd56c54ccd40514332cee59908562
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-01-02 14:05:10 +0700

    new

--- process_question.py
+++ process_question.py
@@ -9,79 +9,57 @@ import sentence_classification
 def get_structure(messages_input):
     result = word_tokenize(messages_input)
     return result
-
-
-
-
-
-
-
-
-
-def get_important_key(messages_input,data):
-    list_key = word_tokenize(messages_input)
-    classes = ""
-    key = ""
-    for key in list_key:
-        for value in data['classes']:
-            if key in value['key']:
-                classes = value['classes']
-                key = key
-    type = ""
-    for key in list_key:
-        for value in data['quection']:
-            if key in value['key']:
-                type = value['type']
-    return classes, key, type            
-
-
-def get_response(classes, key, type, data):
-    response = "NULL"
-    # for value in data['data']:
-    #     print(value['patterns'])
-    return response
-
-
-def get_answer( classes, key, type, data):
-    return get_response(classes, key, type, data)
-
-
-
+         
 def check_story(messages_input, classify):
     print("Checking story")
-    
-    
     data = sentence_classification.load_data('story.json')
     for value in data['data']:
         if classify in value['tag']:
             for result in value['story']:
                 if messages_input in result['patterns']:
                     return result['responses'][0]
-        else:
-            print("Don't have this story")
+    print("Not found in story")
     return "NULL"
+
+
+def get_important_key(messages_input,data):
+    print("")
+    print("Getting important key")
+    stuct_message = get_structure(messages_input)
+    print("struct: ", stuct_message)
+    print("")
+    classes = "NULL"
+    for value in data['classes']:
+        print(value['key']) 
+        if classes in value['key']:
+            classes = value['classes']
+            break
+            
+    print("classes: ", classes)
 
 def process_message_question(messages_input, data):
     # Get the answer to the question
-    
-    classes, key, type = get_important_key(messages_input, data)
-    response = get_response(classes, key, type, data)
-    answer = get_answer(classes, key, type, data)
-    return answer
+    print("")
+    print("Processing question")
+    get_important_key(messages_input, data)
+    return "Chưa biết trả lời câu hỏi này!!!"    
+
 
 
 
 
 def test():
-    messages_input = "tôi muốn hỏi về địa điểm của ngân hàng?"
-    
+    messages_input = "tôi muốn hỏi về địa chỉ của ngân hàng vietcombank?"
+    # messages_input = "khi nào mở cửa ngân hàng?"
     print("messages_input: ", messages_input)
     classify = sentence_classification.analysis_message(messages_input)
     print("classify: ", classify[0])
+    print("")
     result = check_story(messages_input, classify[0])
     if result == "NULL":
         data = sentence_classification.load_data('question.json')
         result = process_message_question(messages_input, data)
+        
     print("bot: ", result)
     
 
--- test.py
+++ test.py
@@ -2,23 +2,5 @@ import sentence_classification
 
 import json
 
-def lower_strings(data):
-    for value in data['data']:
-        for result in value['story']:
-            for i in range(len(result['patterns'])):
-                result['patterns'][i] = result['patterns'][i].lower()
-            for i in range(len(result['responses'])):
-                result['responses'][i] = result['responses'][i].lower()
-    return data                
-                
-data = sentence_classification.load_data('story.json')
+#lower string file data
 
-new_data = lower_strings(data)
-
-
-def write(data):
-    with open("data.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4)
-
-print (new_data)
-write(new_data)
