37f8f833a86ee78b16d050b1e1c91f3f5cf81756
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-01-04 06:08:26 +0700

    new

--- fire_base.py
+++ fire_base.py
@@ -1,3 +1,4 @@
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -6,19 +7,24 @@ import get_message
 
 def random_id():
     return random.randint(100000000, 999999999)
-
 print("Running...")
-
-# Initialize the Firebase Admin SDK and create the db object
-cred = credentials.Certificate("chatappdemo-6a01c-firebase-adminsdk-xnsij-b9101095da.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
 while True:
+
+    cred = credentials.Certificate("chatappdemo-6a01c-firebase-adminsdk-xnsij-b9101095da.json")
+    firebase_admin.initialize_app(cred)
+
+
+    db = firestore.client()
+
+
     messages_ref = db.collection('messages')
+
     docs = messages_ref.stream()
 
+
+
 # sort by timestamp
+
     docs = messages_ref.order_by('timestamp').stream()
     for doc in docs:
         data = doc.to_dict()
@@ -28,13 +34,13 @@ while True:
         print(f"Timestamp: {data['timestamp']}")
     
 # print message last update
-    # print(f"Last message: {data['text']}")    
+    print(f"Last message: {data['text']}")    
 # if the last message have received = True, then send message to firebase
 
     if data['received'] == True:
     # create a new message
         new_message = {
-            'id': random_id(),
+            'ID': random_id(),
             'received': False,
             'text': get_message.get_messge(data['text']),
         # 'timestamp': 2022-11-06 19:56:07.501430+00:00
@@ -42,4 +48,6 @@ while True:
         }
         messages_ref.add(new_message)
 
+
 print("Done")
+
---
+++ get_message.py
@@ -0,0 +1,36 @@
+
+
+import sentence_classification
+import process_question
+import check_story
+from underthesea import text_normalize
+
+def get_message_process(messages_input):
+    classify = sentence_classification.analysis_message(messages_input)
+    result = check_story.get_text(messages_input)
+    if result == "NULL":
+        result = process_question.question(messages_input)
+        return result
+    else:
+        return result
+
+
+# def main():
+#     while True:
+#         # print(intents)
+#         print("Bạn: ", end="")
+#         messages_input = " " + input()
+#         messages_input = text_normalize(messages_input)
+#         if 'quit' in messages_input:
+#             break
+#         else:
+#             replay = get_message_process(messages_input)
+#             print("Bot: ", replay)
+
+
+# main()
+
+def get_messge(messages_input):
+    messages_input = text_normalize(messages_input)
+    relay = get_message_process(messages_input)
+    return relay
